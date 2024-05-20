f656893462df8dd316a4416309d312f93a4b4b2f
Will Mclellarn <will@cardinalecom.com>
2024-05-20 10:53:00 -0600

    lander 1

---
+++ app/home/page.jsx
@@ -0,0 +1,13 @@
+// build a basic page component
+import React from "react";
+import { Homepage } from "@/components/Homepage";
+
+const home = () => {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+};
+
+export default home;
